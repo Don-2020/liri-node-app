305adb645bb16de80403dec8ec10885fc88a5dba
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-09 09:49:10 -0700

    Updated key and liri files

--- liri.js
+++ liri.js
@@ -1,4 +1,21 @@
 // Read and set environment variables
 require("dotenv").config();
-
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+
+
+var spotify = new Spotify(keys.spotify);
+
+var firstCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+switch (firstCommand) {
+    case ('spotify-this-song'):
+        if (secondCommand) {
+            spotifyThisSong(secondCommand)
+        }
+        else {
+            spotifyThisSong("The Sign");
+        }
+    break;
+}
