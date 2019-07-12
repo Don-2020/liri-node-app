fe2ecebc1288add7791193ae68b079f20552a171
Donirvin Dickerson <Dond2018@gmail.com>
2019-07-11 22:03:42 -0700

    Fixed Spotify and OMDB

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 // Read and set environment variables
-conts dotenv = require("dotenv").config();
+require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("../liri-node-app/key");
+var axios = require("axios");
 var request = require('request');
 var fs = require('fs');
 
@@ -9,13 +10,11 @@ var spotify = new Spotify(keys.spotify);
 var omdbKey = keys.omdb.api_key;
 
 var firstCommand = process.argv[2];
-var secondCommand = process.argv[3].split().join(" ");
+var secondCommand = process.argv.slice(3).join(" ");
 
 switch (firstCommand) {
     case ('concert-this'):
-        if (secondCommand) {
-            concertThis(secondCommand)
-        }
+        concertThis(secondCommand)
     case ('spotify-this-song'):
         if (secondCommand) {
             spotifyThisSong(secondCommand)
@@ -30,23 +29,30 @@ switch (firstCommand) {
             findMovie(secondCommand)
         }
         else {
-            findMovie('Mr.Nobody')
+            findMovie("Mr.Nobody")
         }
     case ('do-what-it-says'):
-        doWhatItSay()
+    // doWhatItSay()
 }
 
 function spotifyThisSong(song) {
 
     spotify.search({ type: 'track', query: song, limit: 5 }, function (err, data) {
         if (!err) {
+            
+            // console.log('\n------------------\n\n\n', data.tracks.items[0].album.artists[0].name, "\n\nn "+data.tracks.items[0].album.name)
+            // // console.log('\n------------------\n\n\n', data.tracks.items[0].album.name)
+            // console.log('\n------------------\n\n\n', data.tracks.items[0].preview_url)
+            // console.log('\n------------------\n\n\n', data.tracks.items[0].name)
+            
             for (var i = 0; i < data.tracks.items.length; i++) {
                 var songData = data.tracks.items[i];
-                console.log("Artist: " + songData.artists.name);
+                // console.log("\n--------------------------------\n\n\n")
+                console.log("\nArtist: " + songData.album.artists[0].name);
                 console.log("Song: " + songData.name)
-                console.log("Preview URL: " + songData.external_urls)
+                console.log("Preview URL: " + songData.preview_url)
                 console.log("Album: " + songData.album.name)
-                console.log("-------------------------------------")
+                console.log("\n--------------------------\n\n")
             }
         }
         else {
@@ -60,20 +66,51 @@ function spotifyThisSong(song) {
 
 
 function findMovie(movie) {
+    console.log('MOVIE', movie);
+    console.log('API KEY', omdbKey);
     var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&apikey=' + omdbKey + '&plot=short&tomatoes=true';
+    console.log('URL', omdbURL);
 
-    request(omdbURL, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            var body = JSON.parse(body);
-            console.log("Title: "+ body.Title + "\nReleased Year: "+ body.Year + "\nIMDB Rating: "+ body.imdbRating + "\nRotten Tomatoes Rating: "+ body.tomatoRating + "\nCountry: "+ body.Country + "\nLanguage: "+ body.Langauge + "\nPlot: "+ body.Plot + "\nActors: "+ body.Actors)
-        }
-        else{
-            console.log('Error occured')
-        }
-        if(movie === "Mr.NoBody"){
-            console.log("------------------")
-            console.log("If you haven't watched 'Mr.Nobody,' then you should: http://www.imdb.com/title/tt0485947/ ")
-            console.log("It's on Netflix!")
-        }
-    });
+    axios.get(omdbURL).then(function (response) {
+        console.log(response);
+        console.log("------------------")
+        console.log("Title: " + response.data.Title);
+        console.log("Released Year: " + response.data.Released);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Country: " + response.data.Country);
+        console.log("Language: " + response.data.Langauge);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+        console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating); 
+        console.log("Rotten Tomatoes URL: " + response.data.tomatoURL)
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
+    if (movie === "Mr.NoBody") {
+        console.log("------------------")
+        console.log("If you haven't watched 'Mr.Nobody,' then you should: http://www.imdb.com/title/tt0485947/ ")
+        console.log("It's on Netflix!")
+    }
 }
+//     request(omdbURL, function(error, response, body){
+//         if(!error && response.statusCode === 200){
+//             var responseJSON = response;
+//             console.log("\n------------------\n\n\n" , response)
+//             console.log("\n------------------\n\n\n" , body.Rating`)
+//             console.log("\n------------------\n\n\n" , body.Title, error)
+//             console.log("\n------------------\n\n\n" , response.body.Year, error)
+//             console.log("\n------------------\n\n\n" , responseJSON.body.Title)
+//             console.log("\n------------------\n\n\n" , responseJSON.body.Year, error)
+
+//             // console.log("Title: "+ body.Title);
+//             // console.log("Released Year: "+ body.Year);  
+//             // 
+//         }
+//         else{
+//             console.log('Error occured' + error)
+//         }
+
+//        
+//     });
+// }
